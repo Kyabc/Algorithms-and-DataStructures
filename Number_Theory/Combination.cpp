7a294921cc7462ee6a4e858e3276f9d4215a4cc9
Kyabc <0707.teriyaki@gmail.com>
2020-07-12 16:13:02 +0900

    Update Combination.cpp

--- Number_Theory/Combination.cpp
+++ Number_Theory/Combination.cpp
@@ -2,17 +2,21 @@
 
 template<int mod>
 struct combination {
+private : 
     std::vector<modint<mod>> fact, ifact;
+public :
     combination(int n) : fact(n + 1), ifact(n + 1) {
         fact[0] = 1;
         for (int i = 1; i < n + 1; i++) fact[i] = (fact[i - 1] * i);
         ifact[n] = fact[n].inverse();
         for (int i = n; i > 0; i--) ifact[i - 1] = ifact[i] * i;
     }
+
     modint<mod> operator() (int n, int r) {
         if (r < 0 or r > n) return 0;
         return fact[n] * ifact[n - r] * ifact[r];
     }
+
     constexpr const modint<mod> &operator[] (int i) const {
         return fact[i];
     }
