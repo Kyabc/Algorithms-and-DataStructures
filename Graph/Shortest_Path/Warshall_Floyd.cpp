48c7c7bfabc516612cc724ee18b0fbcb05fc6675
Kyabc <0707.teriyaki@gmail.com>
2020-07-12 21:45:24 +0900

    Update Warshall_Floyd.cpp

--- Graph/Shortest_Path/Warshall_Floyd.cpp
+++ Graph/Shortest_Path/Warshall_Floyd.cpp
@@ -1,12 +1,14 @@
 #include <vector>
 
 template<typename T>
-bool warshall_floyd (std::vector<std::vector<T>> &g) {
+bool warshall_floyd (std::vector<std::vector<T>> &g, const T &inf) {
     const int n = (int)g.size();
+    for (int i = 0; i < n; i++) g[i][i] = T(0);
     bool ret = false;
     for (int k = 0; k < n; k++) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
+                if (g[i][k] == inf or g[k][j] == inf) continue;
                 if (g[i][j] > g[i][k] + g[k][j]) g[i][j] = g[i][k] + g[k][j];
             }
         }
@@ -14,3 +16,6 @@ bool warshall_floyd (std::vector<std::vector<T>> &g) {
     for (int i = 0; i < n; i++) ret |= (g[i][i] < 0);
     return ret;
 }
+
+template<class T>
+using matrix = std::vector<std::vector<T>>;
