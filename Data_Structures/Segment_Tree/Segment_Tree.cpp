4b679f83d2b606e77ebe7ee0b9c3a8ab4b410f8b
Kyabc <0707.teriyaki@gmail.com>
2020-07-30 13:15:11 +0900

    Update Segment_Tree.cpp

--- Data_Structures/Segment_Tree/Segment_Tree.cpp
+++ Data_Structures/Segment_Tree/Segment_Tree.cpp
@@ -4,31 +4,23 @@
 template<class T>
 struct segment_tree {
 	using value_type = T;
+	using size_type = std::size_t;
 	using F = std::function<T(T, T)>;
 private : 
-	int n;
-	F f;
-	value_type ie;
+	size_type n;
+	const F f;
+	const value_type ie;
 	std::vector<value_type> node;
 
 public : 
-	segment_tree () { }
+	segment_tree (const F &f, const value_type &ie) noexcept : f(f), ie(ie) { }
 
-	segment_tree (const F &f, const value_type &ie) : f(f), ie(ie) { }
-
-	void reset (const F &f, const value_type &ie) {
-		(this->f) = f;
-		(this->ie) = ie;
-		node.clear();
+	void assign (size_type _n) noexcept {
+		n = _n;
+		node.assign(n << 1, ie);
 	}
 
-	void assign (int _n) noexcept {
-		n = 1;
-		while (n < _n) n <<= 1;
-		node.assign(2 * n, ie);
-	}
-
-	void assign (int _n, const value_type &x) noexcept {
+	void assign (size_type _n, const value_type &x) noexcept {
 		assign(_n);
 		std::fill(node.begin() + n, node.end(), x);
 		for (int i = n; i--> 0;) {
@@ -37,15 +29,15 @@ public :
 	}
 
 	template<class Iterator>
-	void build (Iterator l, Iterator r) {
-		assign(r - l);
-		for (int i = n; l != r; i++, l++) node[i] = *l;
-		for (int i = n; i--> 0;) {
+	void build (Iterator first, Iterator last) {
+		assign(last - first);
+		for (size_type i = n; first != last; i++, first++) node[i] = (*first);
+		for (int i = n; i --> 0;) {
 			node[i] = f(node[i << 1 | 0], node[i << 1 | 1]);
 		}
 	}
 
-	void set_val (int i, const value_type &x) {
+	void set_val (size_type i, const value_type &x) {
 		i += n;
 		node[i] = x;
 		for (i >>= 1; i > 0; i >>= 1) {
@@ -53,24 +45,24 @@ public :
 		}
 	}
 
-	constexpr value_type fold (int l, int r) {
+	value_type fold (size_type l, size_type r) {
 		value_type vl = ie, vr = ie;
-		for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+		for (l += n, r += n; l ^ r; l >>= 1, r >>= 1) {
 			if (l & 1) vl = f(vl, node[l++]);
 			if (r & 1) vr = f(node[--r], vr);
 		}
 		return f(vl, vr);
 	};
 
-	constexpr const value_type& fold_all() const {
+	constexpr const value_type& fold_all() const noexcept {
 		return (node[1]);
 	}
 
-	constexpr const value_type& operator[] (int i) const {
+	const value_type& operator[] (size_type i) const noexcept {
 		return node[i + n];
 	}
 
-	constexpr const value_type& at (int i) const {
+	const value_type& at (size_type i) const noexcept {
 		return node.at(i + n);
 	}
 
