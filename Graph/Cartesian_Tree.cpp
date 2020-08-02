4c49e41a946e2129f96ba9d33ee8a19e143a415c
Kyabc <0707.teriyaki@gmail.com>
2020-08-02 11:34:56 +0900

    add Cartesian Tree

--- Graph/Cartesian_Tree.cpp
+++ Graph/Cartesian_Tree.cpp
@@ -7,16 +7,16 @@ std::vector<int> make_cartesian_tree (const std::vector<T> &a) {
 	const Compare comp = Compare();
 	const int n = int(a.size());
 	std::vector<int> parent(n, -1);
-	std::stack<int> stack;
+	std::stack<int> right_path;
 	for (int i = 0; i < n; i++) {
 		int left = -1;
-		while (not stack.empty() and comp(a[i], a[stack.top()])) {
-			left = stack.top();
-			stack.pop();
+		while (not right_path.empty() and comp(a[i], a[right_path.top()])) {
+			left = right_path.top();
+			right_path.pop();
 		}
 		if (left != -1) parent[left] = i;
-		if (not stack.empty()) parent[i] = stack.top();
-		stack.push(i);
+		if (not right_path.empty()) parent[i] = right_path.top();
+		right_path.push(i);
 	}
 	return parent;
 }
