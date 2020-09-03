eb6c94278f14cde5982b2ded28811060c83229a0
Kyabc <0707.teriyaki@gmail.com>
2020-09-03 11:02:50 +0900

    update segment tree

--- Data_Structures/Segment_Tree/Segment_Tree.cpp
+++ Data_Structures/Segment_Tree/Segment_Tree.cpp
@@ -1,68 +1,105 @@
 #include <vector>
 #include <functional>
+#include <cstddef>
+#include <cassert>
 
-template<class T, class F>
+template<class T>
 struct segment_tree {
-	using value_type = T;
+	using const_reference = const T&;
 	using size_type = std::size_t;
-private : 
-	size_type n;
-	const F op;
+	using value_type = T;
+	using binary_op = std::function<T(T, T)>;
+private :
+	const binary_op op;
 	const value_type identity;
 	std::vector<value_type> node;
 
-	void calc (size_type i) noexcept {
+	void merge (size_type i) noexcept {
 		node[i] = op(node[i << 1 | 0], node[i << 1 | 1]);
 	}
 
-public : 
-	explicit constexpr segment_tree (const value_type &identity, const F &op) noexcept : identity(identity), op(op) { }
+public :
+	segment_tree (const_reference identity, const binary_op &op)
+	: identity(identity), op(op) { }
 
-	void assign (size_type _size) noexcept {
-		n = _size;
+	segment_tree (size_type n, const_reference identity, const binary_op &op)
+	: node(n << 1, identity), identity(identity), op(op) { }
+
+	segment_tree (size_type n, const_reference value, const_reference identity, const binary_op &op)
+	: node(n << 1, value), identity(identity), op(op) {
+		for (size_type i = n; i > 1; i--) merge(i - 1);
+	}
+
+	template<class InputIterator>
+	explicit segment_tree (InputIterator first, InputIterator last, const_reference identity, const binary_op &op)
+	: node((last - first) << 1), identity(identity), op(op) {
+		assign(last - first);
+		for (size_type i = size(); first != last; i++, first++) node[i] = (*first);
+		for (size_type i = size(); i > 1; i--) merge(i - 1);
+	}
+
+	size_type size () const noexcept {
+		return (node.size() >> 1);
+	}
+
+	void assign (size_type n) noexcept {
 		node.assign(n << 1, identity);
 	}
 
-	void assign (size_type _size, const value_type &value) noexcept {
-		assign(_size);
-		std::fill(node.begin() + n, node.end(), value);
-		for (int i = n; i--> 0;) calc(i);
+	void assign (size_type n, const_reference value) noexcept {
+		assign(n);
+		for (size_type i = n; i < node.size(); i++) node[i] = value;
+		for (size_type i = n; i > 1; i--) merge(i - 1);
 	}
 
 	template<class InputIterator>
 	void assign (InputIterator first, InputIterator last) noexcept {
 		assign(last - first);
-		for (size_type i = n; first != last; i++, first++) node[i] = (*first);
-		for (int i = n; i --> 0;) calc(i);
+		for (size_type i = size(); first != last; i++, first++) node[i] = (*first);
+		for (size_type i = size(); i > 1; i--) merge(i - 1);
 	}
 
-	void set_val (size_type i, const value_type &value) noexcept {
-		node[(i += n)] = value;
-		for (i >>= 1; i > 0; i >>= 1) calc(i);
+	void update (size_type i,const_reference value) noexcept {
+		node[(i += size())] = value;
+		for (i >>= 1; i > 0; i >>= 1) merge(i);
 	}
 
 	value_type fold (size_type first, size_type last) noexcept {
 		value_type vl = identity, vr = identity;
-		for (first += n, last += n; first != last; first >>= 1, last >>= 1) {
+		for (first += size(), last += size(); first != last; first >>= 1, last >>= 1) {
 			if (first & 1) vl = op(vl, node[first++]);
 			if (last  & 1) vr = op(node[--last],  vr);
 		}
 		return op(vl, vr);
-	};
-
-	const value_type& operator[] (size_type i) const noexcept {
-		return node[i + n];
 	}
 
-	const value_type& at (size_type i) const noexcept {
-		return node.at(i + n);
+	const_reference operator[] (size_type i) const noexcept {
+		return node[i + size()];
+	}
+
+	const_reference at (size_type i) const noexcept {
+		assert(i < size());
+		return node[i + size()];
 	}
 
 };
 
+template<class T, class F>
+segment_tree<T> make_segtree (const T &identity, const F &op) {
+	return segment_tree<T>(identity, op);
+}
+
+template<class T, class F>
+segment_tree<T> make_segtree (std::size_t n, const T &identity, const F &op) {
+	return segment_tree<T>(n, identity, op);
+}
+
+template<class T, class F>
+segment_tree<T> make_segtree (std::size_t n, const T &value, const T &identity, const F &op) {
+	return segment_tree<T>(n, value, identity, op);
+}
+
 template<class InputIterator, class T, class F>
-segment_tree<T, F> make_segtree (InputIterator first, InputIterator last, const T &identity, const F &op) {
-	segment_tree<T, F> segtree(identity, op);
-	segtree.assign(first, last);
-	return std::move(segtree);
+segment_tree<T> make_segtree (InputIterator first, InputIterator last, const T &identity, const F &op) {
+	return segment_tree<T>(first, last, identity, op);
 }
