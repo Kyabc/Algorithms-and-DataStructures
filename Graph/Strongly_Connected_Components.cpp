96e4b9569d67c60c2ae5cdad797cafbeccb70898
Kyabc <0707.teriyaki@gmail.com>
2020-09-15 00:51:02 +0900

    update scc

--- Graph/Strongly_Connected_Components.cpp
+++ Graph/Strongly_Connected_Components.cpp
@@ -1,53 +1,70 @@
 #include <vector>
+#include <stack>
+#include <cstddef>
 #include <algorithm>
 
 struct strongly_connected_component {
-private : 
-	std::vector<std::vector<int>> g, revg;
-	std::vector<bool> seen;
-	std::vector<int> order, index;
+	using size_type = std::size_t;
+private :
+	size_type num;
+	std::vector<std::vector<size_type>> g, scc;
+	std::vector<size_type> low, order, index;
+	std::vector<bool> instack;
+	std::stack<size_type> sta;
 
-	void dfs (int v) {
-		seen[v] = true;
-		for (int u : g[v]) {
-			if (not seen[u]) dfs(u);
+	void chmin (size_type &a, const size_type &b) noexcept {
+		if (a > b) a = b;
+	}
+
+	void dfs (size_type v) noexcept {
+		low[v] = order[v] = ++num;
+		sta.push(v); instack[v] = true;
+		for (const size_type &u : g[v]) {
+			if (order[u] == 0) {
+				dfs(u);
+				chmin(low[v], low[u]);
+			} else if (instack[u]) {
+				chmin(low[v], order[u]);
+			}
 		}
-		order.push_back(v);
-	}
-
-	void dfs2 (int v, const int idx) {
-		seen[v] = true;
-		index[v] = idx;
-		for (int u : revg[v]) {
-			if (not seen[u]) dfs2(u, idx);
+		if (low[v] == order[v]) {
+			scc.push_back(std::vector<size_type>());
+			while (true) {
+				const size_type u = sta.top();
+				sta.pop(); instack[u] = false;
+				scc.back().push_back(u);
+				if (u == v) break;
+			}
 		}
 	}
 
-public : 
-	strongly_connected_component (int n) : 
-		g(n), revg(n), index(n), seen(n) { }
+public :
+	explicit strongly_connected_component (size_type n)
+	: num(0), g(n), scc(), low(n, 0), order(n, 0), index(n), instack(n, false), sta() { }
 
-	void add_edge (int u, int v) noexcept {
-		g[u].push_back(v);
-		revg[v].push_back(u);
+	void add_edge (size_type v, size_type u) noexcept {
+		g[v].push_back(u);
 	}
 
-	int build() {
-		order.clear();
-		std::fill(seen.begin(), seen.end(), false);
-		for (int v = 0; v < g.size(); v++) {
-			if (not seen[v]) dfs(v);
+	size_type solve () noexcept {
+		for (size_type v = 0; v < g.size(); v++) if (not order[v]) dfs(v);
+		std::reverse(scc.begin(), scc.end());
+		for (size_type i = 0; i < scc.size(); i++) {
+			for (const size_type &v : scc[i]) index[v] = i;
 		}
-		
-		int idx = 0;
-		std::fill(seen.begin(), seen.end(), false);
-		std::reverse(order.begin(), order.end());
-		for (int v : order) if (not seen[v]) dfs2(v, idx++);
-		return idx;
+		return scc.size();
 	}
 
-	int operator[] (int v) {
-		return (index[v]);
+	const std::vector<std::vector<size_type>> &get_scc () const noexcept {
+		return scc;
+	}
+
+	size_type size () const noexcept {
+		return scc.size();
+	}
+
+	const size_type &operator[] (size_type v) const noexcept {
+		return index[v];
 	}
 
 };
